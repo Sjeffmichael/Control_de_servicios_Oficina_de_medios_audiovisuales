5fc13a202eed0450ef39a87c90f6099e775e3e6e
Sjeffmichael <59579199+Sjeffmichael@users.noreply.github.com>
2021-05-14 22:32:25 -0600

    Initial commit

---
+++ app/src/main/java/com/example/controldeservicios_oficinademediosaudiovisuales/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.controldeservicios_oficinademediosaudiovisuales
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
