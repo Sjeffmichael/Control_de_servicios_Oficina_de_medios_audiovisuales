c29aca9491a527b102a1fa1c29cda2764fff85da
Brandon <brandonzurd23@gmail.com>
2021-05-25 20:40:44 -0600

    nuevos formularios

---
+++ app/EsperaActivity.kt
@@ -0,0 +1,11 @@
+package com.example.controldeservicios_oficinademediosaudiovisuales
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EsperaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registros_espera)
+    }
+}
